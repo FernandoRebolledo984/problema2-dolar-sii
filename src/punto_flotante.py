c7b5ad7556471a4f28c656316230dcd2109fb3bf
FelipeAlonso66 <fvilarroelramosc@gmail.com>
2026-08-31 16:33:39 -0400

    Update punto_flotante.py

--- src/punto_flotante.py
+++ src/punto_flotante.py
@@ -12,8 +12,9 @@ Arreglados_y_Significativos = [float(f"{num:.2g}") for num in datos["dolar_obser
 i = 0
 listaErrores = []
 
-while i <= len(datos["dolar_observado_promedio_clp"]):
+while i < len(datos["dolar_observado_promedio_clp"]):
     listaErrores.append(listaOriginales[i] - Arreglados_y_Significativos[i])
+    i = i+1
     pass
 
 print(listaErrores)
