7a79c8a1352222c8242c08d3f52894c702b6a4ca
FelipeAlonso66 <fvilarroelramosc@gmail.com>
2026-08-31 16:03:32 -0400

    Update punto_flotante.py

--- src/punto_flotante.py
+++ src/punto_flotante.py
@@ -4,6 +4,5 @@ import os
 os.system('cls' if os.name == 'nt' else 'clear')
 print(datos["dolar_observado_promedio_clp"])
 
-
 Arreglados_y_Significativos = [[float(f"{num:.2g}") for num in datos["dolar_observado_promedio_clp"]]]
 print(Arreglados_y_Significativos)
