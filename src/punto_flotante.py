33d920b103a7dee4590d89129f1ddd52dd0ebb9d
FelipeAlonso66 <fvilarroelramosc@gmail.com>
2026-08-31 15:46:37 -0400

    Update punto_flotante.py

--- src/punto_flotante.py
+++ src/punto_flotante.py
@@ -1,10 +1,10 @@
 from cargar_datos import datos
 import os
-os.system('cls' if os.name == 'nt' else 'clear')
 
+os.system('cls' if os.name == 'nt' else 'clear')
 print(datos["dolar_observado_promedio_clp"])
 listaArreglados = []
-print(listaArreglados)
+
 for linea in datos["dolar_observado_promedio_clp"]:
     linea_str = str(linea)
     if len(linea_str) <= 6:
@@ -19,3 +19,8 @@ for linea in datos["dolar_observado_promedio_clp"]:
             listaArreglados.append(aux)
         else:
             listaArreglados.append(float(linea))
+
+print(listaArreglados)
+print(type(listaArreglados[0]))
+for num in listaArreglados:
+    pass
