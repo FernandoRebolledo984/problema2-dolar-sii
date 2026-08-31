7a4454a78de9645ec2e012758220624ba807ff45
FelipeAlonso66 <fvilarroelramosc@gmail.com>
2026-08-31 16:24:35 -0400

    Update punto_flotante.py

--- src/punto_flotante.py
+++ src/punto_flotante.py
@@ -1,8 +1,19 @@
 from cargar_datos import datos
 import os
-
+#original - aproximacion
 os.system('cls' if os.name == 'nt' else 'clear')
 print(datos["dolar_observado_promedio_clp"])
 
-Arreglados_y_Significativos = [[float(f"{num:.2g}") for num in datos["dolar_observado_promedio_clp"]]]
-print(Arreglados_y_Significativos)
+listaOriginales = []
+
+for num in datos["dolar_observado_promedio_clp"]:
+    listaOriginales.append(float(num))
+
+Arreglados_y_Significativos = [float(f"{num:.2g}") for num in datos["dolar_observado_promedio_clp"]]
+i = 0
+listaErrores = []
+
+for og in listaOriginales:
+        listaErrores.append(og - Arreglados_y_Significativos[og])
+
+print(listaErrores)
