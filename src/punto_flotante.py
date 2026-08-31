f3d4ba3911cbf88f60f0af53422dcf539fcfe255
FernandoRebolledo984 <fernandorebolledo98441@gmail.com>
2026-08-30 20:11:01 -0400

    Update punto_flotante.py

--- src/punto_flotante.py
+++ src/punto_flotante.py
@@ -1,3 +1,13 @@
 from cargar_datos import datos
 
-print(datos)
+print(datos["dolar_observado_promedio_clp"])
+
+for linea in datos["dolar_observado_promedio_clp"]:
+
+    if len(str(linea)) <= 6:
+        auxLine = 0
+        if int(linea)[2] >= 5:
+            auxLine = linea+10
+            print(auxLine)
+    else:
+        pass
