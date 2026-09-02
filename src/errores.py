1733b04b062020f9865c269731a44dc646be8530
FelipeAlonso66 <fvilarroelramosc@gmail.com>
2026-09-01 20:40:33 -0400

    Update errores.py

--- src/errores.py
+++ src/errores.py
@@ -3,3 +3,13 @@ import os
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
+valoeVerdadero = listaOriginales
+errorAbsoluto = listaErrores
+errorRelativo = []
+
+i = 0
+while i < len(Arreglados_y_Significativos):
+    errorRelativo.append((errorAbsoluto[i]/valoeVerdadero[i])*100)
+    i = i+1
+
+
