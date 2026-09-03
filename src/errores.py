0a7b4cc9ea9e8ee9cdec2fb34d57fe8da9f26932
FelipeAlonso66 <fvilarroelramosc@gmail.com>
2026-09-03 13:41:55 -0400

    Update errores.py

--- src/errores.py
+++ src/errores.py
@@ -10,14 +10,4 @@ while i < len(Arreglados_y_Significativos):
     errorRelativo.append((errorAbsoluto[i]/valorVerdadero[i])*100)
     i = i+1
 
-##OPERACIONES DE PROPAGACIÓN (UNA DE CADA UNA)##
-
-print(Arreglados_y_Significativos)
-
-def comprar_dolares(monto, precio):
-    dollars = monto / precio
-    return dollars
-
-montoTrabajo = 1000000
-
-usd = comprar_dolares(montoTrabajo, Arreglados_y_Significativos)            
+##OPERACIONES DE PROPAGACIÓN (UNA DE CADA UNA)##          
