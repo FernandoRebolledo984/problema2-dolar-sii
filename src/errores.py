b8e45c9234d261300d33f60e56af2a36fc757967
FernandoRebolledo984 <fernandorebolledo98441@gmail.com>
2026-09-07 20:37:43 -0300

    Update errores.py

--- src/errores.py
+++ src/errores.py
@@ -10,11 +10,11 @@ while i < len(Arreglados_y_Significativos):
     errorRelativo.append((errorAbsoluto[i]/valorVerdadero[i])*100)
     i = i+1
 
-print("\n=== Respuesta A1. Error de representación mes a mes")
+print("\n=== Respuesta A1. Error de representación mes a mes ===")
 print("\n|= Precios =|= Error absoluto =|= Error porcentual =|")
 i = 0
 while i < len(Arreglados_y_Significativos):
-    print(f"|=  {Arreglados_y_Significativos[i]}  =|=      {round(errorAbsoluto[i],2)}      =|=       {round(errorRelativo[i],2)}       =|")
+    print(f"|=  {Arreglados_y_Significativos[i]}  =|=      {round(errorAbsoluto[i],3)}      =|=       {round(errorRelativo[i],3)}      =|")
     i+=1
 
 #################################################OPERACIONES DE PROPAGACIÓN#################################################
@@ -35,7 +35,7 @@ precio_venta = Arreglados_y_Significativos[6]   # Julio 2022
 pesos_final = usd_comprados * precio_venta
 
 errorPesoFinal = errorDolar + errorVenta #Suma de errores relativos de ambos meses
-print(f"Los USD comprados en Enero son: {round(usd_comprados,2)}\nLos USD vendidos (en pesos) en Julio son: {round(pesos_final,2)}")
+print(f" - Los USD comprados en Enero son: {round(usd_comprados,3)}\n - Los USD vendidos (en pesos) en Julio son: {round(pesos_final,3)}")
 
 #Ganancia real
 ganancia_real = pesos_final - monto
@@ -45,14 +45,14 @@ errorAbsolutoPesoFinal = (errorPesoFinal/100)*pesos_final
 errorGanancia = errorAbsolutoPesoFinal + 0 #0 otra vez por el error de monto que es 0
 porcentajeErrorGanancia = (errorGanancia/ganancia_real)*100
 
-print(f"Por lo que las ganancias son: {ganancia_real:.2f} +/- {round(errorGanancia,2)} (o +/- {porcentajeErrorGanancia:.3g}%).")
+print(f" - Por lo que las ganancias son: {ganancia_real:.2f} +/- {round(errorGanancia,3)} (o +/- {porcentajeErrorGanancia:.3g}%).")
 
 ##################CANCELACIÓN##################
 #Diciembre 2022 - diciembre 2023
-dif_Dic1_Dic2 = f"{valorVerdadero[23] - valorVerdadero[11]:.2g}"
+dif_Dic1_Dic2 = f"{valorVerdadero[23] - valorVerdadero[11]:.3g}"
+dif_Dic1_Dic2_fix = float(dif_Dic1_Dic2)
 propAbs_Dic1_Dic2 = errorAbsoluto[23] + errorAbsoluto[11]
 errorPrcntl_Dic1_Dic2 = (propAbs_Dic1_Dic2/float(dif_Dic1_Dic2))*100
 print("\n\n=== Respuesta A3. Cancelación ===\n")
-print(f"== Diferencia entre ambos meses: {dif_Dic1_Dic2}\n== Error absoluto (+/-): {round(propAbs_Dic1_Dic2,2)} ")
-print("== Debido a que la diferencia verdadera entre ambos meses es menor al error absoluto se puede concluir que no se puede afirmar\n" \
-"subió o bajó.")
+print(f" - Diferencia entre ambos meses: {abs(dif_Dic1_Dic2_fix)}\n - Error absoluto (+/-): {round(propAbs_Dic1_Dic2,3)} ")
+print(" - Debido a que la diferencia verdadera entre ambos meses es menor al error absoluto se puede concluir que no se puede afirmar si subió o bajó.\n")
